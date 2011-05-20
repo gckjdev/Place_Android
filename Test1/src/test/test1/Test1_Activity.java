01205b66da16117c712986c303de7a76cf657eeb
ouyzhu <ouyzhu@gmail.com>
2011-05-20 14:05:12 +0800

    Init Commit for Test project

---
+++ Test1/src/test/test1/Test1_Activity.java
@@ -0,0 +1,13 @@
+package test.test1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test1_Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main); 
+    }
+}
